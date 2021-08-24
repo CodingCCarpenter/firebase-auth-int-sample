e43592cf18da2250c2efe094cf531e83787cacb2
Tim Shaker <tim@tmshkr.com>
2021-08-24 16:10:42 -0700

    set up firebase auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase/app";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+const uiConfig = {
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  },
+};
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        setIsSignedIn(!!user);
+      });
+    return () => unregisterAuthObserver();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isSignedIn ? (
+          <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
       </header>
     </div>
   );
---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCns7rjM4fwG6jRng_o0f31fE6NKiYZXEo",
+  authDomain: "mentor-beast-f190c.firebaseapp.com",
+  projectId: "mentor-beast-f190c",
+  storageBucket: "mentor-beast-f190c.appspot.com",
+  messagingSenderId: "376697092042",
+  appId: "1:376697092042:web:d78063fcc4d68c9213c133",
+  measurementId: "G-X0NYTW31D0",
+};
