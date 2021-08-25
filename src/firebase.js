cfb6cb0fb3a2c27ac246dbb5487c9ba40abca9a5
Tim Shaker <tim@tmshkr.com>
2021-08-25 00:55:47 -0700

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyCns7rjM4fwG6jRng_o0f31fE6NKiYZXEo",
@@ -8,3 +11,13 @@ export const firebaseConfig = {
   appId: "1:376697092042:web:d78063fcc4d68c9213c133",
   measurementId: "G-X0NYTW31D0",
 };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
+export const rtdb = firebase.database();
+export const auth = firebase.auth();
+if (window.location.hostname === "localhost") {
+  rtdb.useEmulator("localhost", 9000);
+  auth.useEmulator("http://localhost:9099");
+}
