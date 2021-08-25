d9289a02576fbaf22248a6def090cfff9f497c1c
Tim Shaker <tim@tmshkr.com>
2021-08-25 00:55:52 -0700

    check db read

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import firebase from "firebase/app";
+import firebase, { rtdb } from "./firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 const uiConfig = {
@@ -18,9 +18,28 @@ function App() {
     const unregisterAuthObserver = firebase
       .auth()
       .onAuthStateChanged((user) => {
+        console.log(user);
         setIsSignedIn(!!user);
       });
     return () => unregisterAuthObserver();
+  }, []);
+
+  useEffect(() => {
+    const email = "tim@nuclius,com";
+    rtdb
+      .ref("invited_users/")
+      .child(email)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+        } else {
+          console.log("User not invited");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
   return (
