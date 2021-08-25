3898a4d69ddffb92c0e374e8199fd8242a93fcfd
Tim Shaker <tim@tmshkr.com>
2021-08-25 00:55:19 -0700

    checkInvited

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,8 @@
 import * as functions from "firebase-functions";
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const database = admin.database();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +11,18 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+export const checkInvited = functions.auth
+  .user()
+  .onCreate(async (user: any) => {
+    const email = user.email.replace(".", ",");
+    const invited = await database
+      .ref("invited_users/")
+      .child(email)
+      .get()
+      .then((snapshot) => !!snapshot.val())
+      .catch(console.error);
+    console.log(invited);
+    await admin.auth().setCustomUserClaims(user.uid, { invited });
+    return invited;
+  });
