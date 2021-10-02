cb69bd6d2b00c110e9bddcfb44bf3f8d88403e20
Tim Shaker <tim@tmshkr.com>
2021-10-01 22:11:57 -0700

    delete unused code

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCns7rjM4fwG6jRng_o0f31fE6NKiYZXEo",
-  authDomain: "mentor-beast-f190c.firebaseapp.com",
-  projectId: "mentor-beast-f190c",
-  storageBucket: "mentor-beast-f190c.appspot.com",
-  messagingSenderId: "376697092042",
-  appId: "1:376697092042:web:d78063fcc4d68c9213c133",
-  measurementId: "G-X0NYTW31D0",
+  // Your web app's Firebase configuration
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -17,7 +11,3 @@ firebase.initializeApp(firebaseConfig);
 export default firebase;
 export const rtdb = firebase.database();
 export const auth = firebase.auth();
-// if (window.location.hostname === "localhost") {
-//   rtdb.useEmulator("localhost", 9000);
-//   auth.useEmulator("http://localhost:9099");
-// }
